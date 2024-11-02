d5ec3408708e335e2535f2e53778264d700d9060
dwnppo <dwnppoalt@outlook.com>
2024-11-02 11:04:54 +0800

    rewtire of s3 object, use aws-sdk v2 isntead of v3 because wercel doesnt like it tt~tt

--- public/scripts/s3.js
+++ public/scripts/s3.js
@@ -1,20 +1,18 @@
-const { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand} = require('@aws-sdk/client-s3');
-const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 require('dotenv').config();
 
 class S3 {
     constructor() {
-        this.s3Client = new S3Client(
-            {
-                region: process.env.S3_REGION,
-                credentials: {
-                    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-                    secretAccessKey: process.env.S3_ACCESS_KEY_SECRET
-                }
-            }
-        );
-        console.log("S3 initialized.")
         this.bucketName = process.env.BUCKET_NAME;
+        this.AWS = AWS.config.update({
+            region: process.env.S3_REGION,
+            accessKeyId: process.env.S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.S3_ACCESS_KEY_SECRET
+        })
+        this.s3Client = new AWS.S3();
+        console.log("S3 initialized.")
+        
     }
     /**
      * Uploads the file to the S3 defined by the .env file.
@@ -22,7 +20,7 @@ class S3 {
      * @param {Buffer | Uint8Array | Blob | string} params.fileContent - file data
      * @param {string} params.key - unique version 4 UUID for the file name.
      */
-    async uploadFile(params) {
+    uploadFile(params) {
         const { fileContent, key } = params
         const uploadParams = {
             Bucket: this.bucketName,
@@ -31,7 +29,7 @@ class S3 {
             ContentType: 'application/pdf'
         }
         try {
-            await this.s3Client.send(new PutObjectCommand(uploadParams));
+            this.s3Client.putObject(uploadParams);
             console.log(`Upload of file: ${key}.pdf successful`);
         } catch (err) {
             console.error("Error: ", err);
@@ -43,16 +41,17 @@ class S3 {
      * @param {Object} params - parameters for the function.
      * @param {string} params.key - unique version 4 UUID to access the file.
      * @param {number} params.expiresIn - expiration time for the pre-signed URL. Default: 3600 seconds or 1 hour.
-     * @returns {Promise<string>} - the pre-signed URL that the user can access.
+     * @returns {string} - the pre-signed URL that the user can access.
      */
-    async getAccessLink(params) {
+    getAccessLink(params) {
         const { key, expiresIn } = params
         const getFileParams = {
             Bucket: this.bucketName,
-            Key: `${key}.pdf`
+            Key: `${key}.pdf`,
+            Expires: expiresIn
         };
         try {
-            const url = await getSignedUrl(this.s3Client, new GetObjectCommand(getFileParams), { expiresIn });
+            const url = this.s3Client.getSignedUrl('getObject', getFileParams)
             console.log(`URL for file: ${key} is created. URL: ${url}`);
             return url;
         } catch (err) {
@@ -64,18 +63,18 @@ class S3 {
      * Deletes the file determined by params.uuid, the name of the file.
      * @param {string} uuid - unique version 4 UUID to access the file.
      */
-    async deleteFile(uuid) {
+    deleteFile(uuid) {
         const deleteFileParams = {
             Bucket: this.bucketName,
             Key: `${uuid}.pdf`
         }
-        try {
-            await this.s3Client.send(new DeleteObjectCommand(deleteFileParams))
-            console.log(`File ${uuid}.pdf deleted.`);
-        } catch (err) {
-            console.error('Error: ', err)
-        }
+        this.s3Client.deleteObject(deleteFileParams, (err, data) => {
+            if (err) {
+                console.error(`Error deleting object: ${err}`)
+            } else {
+                console.log(`Successfully deleted object: ${data}`);
+            }
+        })
     }
 }
 
-module.exports = S3;
