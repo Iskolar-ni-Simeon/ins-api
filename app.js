71714d2e2b81aab125819e0ee9b8ea63d5e07a1c
dwnppo <dwnppoalt@outlook.com>
2024-11-03 22:05:35 +0800

    removal of comments

--- app.js
+++ app.js
@@ -47,25 +47,6 @@ const B2Class = new B2();
 console.log("Initializing SQL...");
 const SQLClass = new SQL();
 
-/**
- * to-do: 
- * /login - POST
- * 
- * USER-RELATED ROUTES:
- * /adduser - POST
- * /userlibrary - POST (for adding) & GET (for getting, no shit sherlock)
- * 
- * THESIS-RELATED ROUTES:
- * /search - GET
- * /thesis - POST (for adding, again) & GET (for getting info, again, no shit) /upload - POST
- * /delete - POST (isama na dito yung pagdelete ng file sa S3. )
- * 
- * FILE-RELATED ROUTES:
- * /accessthesis - GET (gumawa na lang ng button na "Access this file" para hindi constantly kumukuha ng link sa S3.)
- * 
- * DON'T FORGET: ILAGAY YUNG decodeJWT MIDDLEWARE PLSPLSPLSPLSPLSPLS
- * MAG-GENERATE NG BAGONG BEARER TOKEN SA 'Iskolar ni Simeon'.
- */
 initializeKeys().then(() => {
     console.log(userJWT);
     console.log('keys initialized')
@@ -74,7 +55,6 @@ initializeKeys().then(() => {
     require('./routes/userRoute.js')(app, B2Class, SQLClass, JWTMiddleware, publicKey);
 
 })
-
 
 app.listen(PORT, function() {
     console.log(`Listening at port ${PORT}`);
