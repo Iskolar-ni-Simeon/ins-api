d1f1f61dddfc070d37586ecbb7e09a061d5ac6e7
dwnppo <dwnppoalt@outlook.com>
2024-11-03 22:20:28 +0800

    moved app.js to /api/app.js

--- api/app.js
+++ api/app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const B2 = require('./public/scripts/b2.js');
-const SQL = require('./public/scripts/sql.js');
-const { generateKey, JWTMiddleware } = require('./public/scripts/auth.js');
+const B2 = require('../public/scripts/b2.js');
+const SQL = require('../public/scripts/sql.js');
+const { generateKey, JWTMiddleware } = require('../public/scripts/auth.js');
 
 const app = express();
 const PORT = 5000;
@@ -53,12 +53,16 @@ initializeKeys().then(() => {
     app.use('/', (req, res, next) => {
         res.send("Hello, world!");
     });
-    require('./routes/authenticationRoute.js')(app, privateKey, SQLClass);
-    require('./routes/thesisRoute.js')(app, B2Class, SQLClass, JWTMiddleware, publicKey);
-    require('./routes/userRoute.js')(app, B2Class, SQLClass, JWTMiddleware, publicKey);
+    require('../routes/authenticationRoute.js')(app, privateKey, SQLClass);
+    require('../routes/thesisRoute.js')(app, B2Class, SQLClass, JWTMiddleware, publicKey);
+    require('../routes/userRoute.js')(app, B2Class, SQLClass, JWTMiddleware, publicKey);
 
 })
 
 app.listen(PORT, function() {
     console.log(`Listening at port ${PORT}`);
 });
+
+module.exports = (req, res) => {
+    app(req, res);
+};
