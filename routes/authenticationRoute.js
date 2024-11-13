e3198c9c5f1193cfd58bf12bc0a4ca217caa802f
dwnppo <dwnppoalt@outlook.com>
2024-11-13 19:51:09 +0800

    share public key as a result in /login

--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -31,7 +31,7 @@ module.exports = (app, privateKey, sql) => {
             console.log(decodedToken);
             await sql.addUser({id: verifiedToken.sub, email: verifiedToken.email, name: verifiedToken.name});
             const savedTheses = await sql.getUserSavedTheses(verifiedToken.sub);
-            return res.json({ token: verifiedToken, jwtToken: userJWT, saved: savedTheses});
+            return res.json({ token: verifiedToken, jwtToken: userJWT, saved: savedTheses, publicKey: publicKeyPem});
         } catch (err) {
             console.error('Token validation error: ', err);
             return res.status(500).json({ error: err.message });
