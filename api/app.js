969408ffa51c080787649e1ec6b69c352c7a31f1
dwnppo <dwnppoalt@outlook.com>
2024-11-22 00:02:47 +0800

    fix: change cors

--- api/app.js
+++ api/app.js
@@ -12,29 +12,28 @@ const publicKey = process.env.PUBLIC_KEY.replace(/\\n/g, '\n');
 const app = express();
 const PORT = 5000;
 
-const allowedOrigins = [
-    'http://localhost:8080',
-    'https://iskolar-ni-simeon.vercel.app'
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, origin);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
 
 app.use(cors(corsOptions)); 
 app.use(express.json());
-app.options('*', cors(corsOptions));
+app.use(cors())
 app.use(cookieParser());
-
+if (process.env.NODE_ENV === "development"){
+    app.use(
+      cors({
+        origin: "https://localhost:8080",
+        credentials: true,
+      })
+    );
+  }
+  
+  if (process.env.NODE_ENV === "production"){
+    app.use(
+      cors({
+        origin: "https://iskolar-ni-simeon.vercel.app",
+        credentials: true,
+      })
+    );
+  }
 console.log("Initializing B2...");
 const B2Class = new B2();
 console.log("Initializing SQL...");
