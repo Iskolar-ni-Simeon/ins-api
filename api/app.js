3dade41a7cca2a95ae0882e1b7b1fdf6e7691fa9
dwnppo <dwnppoalt@outlook.com>
2024-11-22 00:15:18 +0800

    fix: add access-control-allow-credentials to be true

--- api/app.js
+++ api/app.js
@@ -32,6 +32,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.options('*', cors(corsOptions));
 app.use(cookieParser());
 
